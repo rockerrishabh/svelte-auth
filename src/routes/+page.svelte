2aff0a0d8e1b55310caa083d21d2af7052aa5536
Rishabh Kumar <rishabh.6542@gmail.com>
2025-03-28 16:49:22 +0530

    fixed auth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,10 @@
 	import { SignIn } from '@auth/sveltekit/components';
 </script>
 
+<svelte:head>
+	<title>Blog App</title>
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
 <a href="/dashboard">Dashboard</a>
 <SignIn provider="github" signInPage="signin" options={{ redirectTo: '/dashboard' }} />
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -2,5 +2,9 @@
 	import { page } from '$app/state';
 </script>
 
+<svelte:head>
+	<title>Dashboard</title>
+</svelte:head>
+
 <h1>Dashboard</h1>
 <p>{page.data.session?.user?.name}</p>
