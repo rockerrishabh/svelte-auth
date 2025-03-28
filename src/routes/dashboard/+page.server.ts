864055f475efa8dac065f0799c35fe7d1ffed876
Rishabh Kumar <rishabh.6542@gmail.com>
2025-03-28 16:40:50 +0530

    fixed auth

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -4,6 +4,8 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async (event) => {
 	const session = await event.locals.auth();
 
+	console.log(session);
+
 	if (!session?.user?.id) {
 		return redirect(303, `/`);
 	}
